5d4952eb5da07b5869ede3f44f5e926fa6be1285
disisK <126535872+disisK@users.noreply.github.com>
2023-04-25 11:55:24 +0900

    Add files via upload

---
+++ GFA1_YUTIG/script.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("mySidebar").style.width = "100px";
+    document.getElementById("myNav").style.marginRight = "100px";
+}
+  
+function closeNav(){
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("myNav").style.marginRight = "0";
+
+}
